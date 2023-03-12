3d3dc019195107bb3cf1b8b4a9247f7352e860fa
Ayimany <ayimany.hub@gmail.com>
2023-03-11 22:27:11 -0600

    [???] Unstaged changes (Which I swear I staged) which did not pass through to the last commit

--- src/core/Robot.hh
+++ src/core/Robot.hh
@@ -7,37 +7,27 @@
 
 #include "RobotContainer.hh"
 
-#include "motorcontrollers/builders/REV/CANSparkMax.hh"
-#include "encoders/builders/REV/SparkMaxRelativeEncoder.hh"
-
 class Robot : public frc::TimedRobot {
- public:
-  void RobotInit() override;
-  void RobotPeriodic() override;
-  void DisabledInit() override;
-  void DisabledPeriodic() override;
-  void DisabledExit() override;
-  void AutonomousInit() override;
-  void AutonomousPeriodic() override;
-  void AutonomousExit() override;
-  void TeleopInit() override;
-  void TeleopPeriodic() override;
-  void TeleopExit() override;
-  void TestInit() override;
-  void TestPeriodic() override;
-  void TestExit() override;
+    friend RobotContainer;
+  
+    public:
+        void RobotInit() override;
+        void RobotPeriodic() override;
+        void DisabledInit() override;
+        void DisabledPeriodic() override;
+        void DisabledExit() override;
+        void AutonomousInit() override;
+        void AutonomousPeriodic() override;
+        void AutonomousExit() override;
+        void TeleopInit() override;
+        void TeleopPeriodic() override;
+        void TeleopExit() override;
+        void TestInit() override;
+        void TestPeriodic() override;
+        void TestExit() override;
 
- private:
-  std::optional<frc2::CommandPtr> m_autonomousCommand;
+    private:
+        std::optional<frc2::CommandPtr> m_autonomousCommand;
+        RobotContainer m_container;
 
-  std::shared_ptr<ffrc::devices::motorcontrollers::CANSparkMax> controller =
-    ffrc::builders::motorcontrollers::CANSparkMax()
-    .Id(3)
-    ->Build();
-
-  std::shared_ptr<ffrc::devices::encoders::SparkMaxRelativeEncoder> encoder =
-    ffrc::builders::encoders::SparkMaxRelativeEncoder(controller)
-    .Build();
-
-  RobotContainer m_container;
 };
